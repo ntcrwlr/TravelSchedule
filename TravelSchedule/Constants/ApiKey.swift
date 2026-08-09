71cc5c356f362325365c45919fcba6e089aa4492
Serg <bushkov.s.p@icloud.com>
2026-08-09 23:12:30 +0300

    feat: add OpenAPI specification and API services

--- TravelSchedule/Constants/ApiKey.swift
+++ TravelSchedule/Constants/ApiKey.swift
@@ -1,3 +1,3 @@
 enum ApiKey {
-    static let yandexRasp = "a26d6798-c48f-455f-9fc8-25bcec1ff9b4"
+    static let yandexRasp = "мой апи"
 }
--- TravelSchedule/Services/NearestStationsService.swift
+++ TravelSchedule/Services/NearestStationsService.swift
@@ -1,31 +1,16 @@
-//
-//  NearestStationsService.swift.swift
-//  TravelSchedule
-//
-//  Created by Сергей Бушков on 09.08.2026.
-//
 import Foundation
-// 1. Импортируем библиотеки:
 import OpenAPIRuntime
 import OpenAPIURLSession
 
-// 2. Улучшаем читаемость кода — необязательный шаг
-// Создаём псевдоним (typealias) для сгенерированного типа Stations.
-// Полное имя Components.Schemas.Stations соответствует пути в openapi.yaml:
-// components → schemas → Stations
 typealias NearestStations = Components.Schemas.NearestStationsResponse
 
-// Определяем протокол для нашего сервиса (хорошая практика для тестирования и гибкости)
 protocol NearestStationsServiceProtocol {
-  // Функция для получения станций, асинхронная и может выбросить ошибку
+ 
   func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations
 }
 
-// Конкретная реализация сервиса
 final class NearestStationsService: NearestStationsServiceProtocol {
-  // Хранит экземпляр сгенерированного клиента
   private let client: Client
-  // Хранит API-ключ (лучше передавать его извне, чем хранить прямо в сервисе)
   private let apikey: String
   
   init(client: Client, apikey: String) {
@@ -34,18 +19,12 @@ final class NearestStationsService: NearestStationsServiceProtocol {
   }
   
   func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations {
-    // Вызываем функцию getNearestStations на ЭКЗЕМПЛЯРЕ сгенерированного клиента.
-    // Имя функции и параметры 'query' напрямую соответствуют операции
-    // 'getNearestStations' и её параметрам в openapi.yaml
     let response = try await client.getNearestStations(query: .init(
-        apikey: apikey,     // Передаём API-ключ
-        lat: lat,           // Передаём широту
-        lng: lng,           // Передаём долготу
-        distance: distance  // Передаём дистанцию
+        apikey: apikey,
+        lat: lat,
+        lng: lng,
+        distance: distance
     ))
-    // response.ok: Доступ к успешному ответу
-    // .body: Получаем тело ответа
-    // .json: Получаем объект из JSON в ожидаемом типе NearestStations
     return try response.ok.body.json
   }
 }
